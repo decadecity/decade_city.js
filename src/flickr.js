4ad0896bdc3405a6d388418a6756c6860c6df3a9
Orde Saunders <orde@orde.org.uk>
2013-04-22 13:49:57 +0100

    Updated flickr module to work with new items.

--- src/flickr.js
+++ src/flickr.js
@@ -7,7 +7,11 @@ window.DECADE_CITY = (function (module, $) {
   module.FLICKR = (function (module, submodule, $) {
     var image_replace = /^http(s)?:\/\/(.*)\.staticflickr.com\/(.*?)(_.\.jpg|\.jpg)$/, // Regex to break up a flickr image URL.
         getInt,
-        responsiveImages;
+        responsiveImages,
+        imageSrc,
+        init,
+        flickr_suffix = '_m', // Default to suffix of smallest image.
+        flickr_suffix_set = false;  //Has the flickr suffix been set? {Boolean}
 
     /**
      * Sanitiser for parseInt that will cope with NaN.
@@ -23,32 +27,20 @@ window.DECADE_CITY = (function (module, $) {
     };
 
     /**
-     * Suffix for Flckr images. {String}
-     */
-    submodule._flickr_suffix = '_m'; // Default to suffix of smallest image. Public for testing.
-
-    /**
-     * Has the flickr suffix been set? {Boolean}
-     */
-    submodule._flickr_suffix_set = false; //Public for testing.
-
-    /**
      * Replaces a flickr image URL with the suffix set for the environment.
-     *
-     * Public for testing.
      *
      * @param src {String} Flickr image URL.
      *
      * @return {String} Image with flickr suffix set.
      */
-    submodule._imageSrc = function (src) {
+    imageSrc = function (src) {
       var match, secure;
       match = image_replace.exec(src);
       if (!match) {
         return src;
       }
       secure = match[1] || '';
-      return 'http' + secure + '://' + match[2] + '.staticflickr.com/' + match[3] + submodule._flickr_suffix + '.jpg';
+      return 'http' + secure + '://' + match[2] + '.staticflickr.com/' + match[3] + flickr_suffix + '.jpg';
     };
 
     /**
@@ -75,7 +67,7 @@ window.DECADE_CITY = (function (module, $) {
         images.each(function () {
           var content_img = $(this), // Re-scope this.
               src = content_img.attr('src'),
-              new_src = submodule._imageSrc(src), // URL of the replacement image.
+              new_src = imageSrc(src), // URL of the replacement image.
               cache_img; // Cache image we will use to load the new image.
           if (src === new_src) {
             // Nothing doing.
@@ -105,16 +97,15 @@ window.DECADE_CITY = (function (module, $) {
      * @param pixel_density {Number} Pixel density of display.
      * @param speed {String} Connection speed [fast|slow].
      */
-    submodule.init = function (width, height, pixel_density, speed) {
+    init = function (width, height, pixel_density, speed) {
       var window_width;
 
-      speed = speed || 'slow'; // Hard coded override for now.
 
-      if (typeof module.SPEED_TEST === 'object' && typeof module.SPEED_TEST.load_speed !== 'undefined') {
-        speed = module.SPEED_TEST.load_speed;
+      if (typeof module.load_speed !== 'undefined' && typeof speed === 'undefined') {
+        speed = module.load_speed;
       }
 
-      if (!submodule._flickr_suffix_set) {
+      if (!flickr_suffix_set) {
         // This has already been run so don't do it again.
 
         width = width || $(window).width();
@@ -130,32 +121,41 @@ window.DECADE_CITY = (function (module, $) {
 
         // This sorts out the flickr naming convention based on image width.
         if (window_width < 280 || speed === 'slow') {
-          submodule._flickr_suffix = '_m';
+          flickr_suffix = '_m';
         } else if (window_width < 320) {
-          submodule._flickr_suffix = '_n';
+          flickr_suffix = '_n';
         } else if (window_width < 500) {
-          submodule._flickr_suffix = '';
+          flickr_suffix = '';
         } else if (window_width < 640) {
-          submodule._flickr_suffix = '_z';
+          flickr_suffix = '_z';
         } else if (window_width < 800) {
-          submodule._flickr_suffix = '_c';
+          flickr_suffix = '_c';
         } else {
-          submodule._flickr_suffix = '_b';
+          flickr_suffix = '_b';
         }
         if (typeof module.PROFILE.profile === 'object') {
           // Store this in the profile.
-          module.PROFILE.profile.flickr_suffix = submodule._flickr_suffix;
+          module.PROFILE.profile.flickr_suffix = flickr_suffix;
         }
         if (typeof module.COOKIES !== 'undefined') {
           // Set a cookie so we can do some of this work on the server.
-          module.COOKIES.setItem('flickr_suffix', submodule._flickr_suffix, null, '/');
+          module.COOKIES.setItem('flickr_suffix', flickr_suffix, null, '/');
         }
-        submodule._flickr_suffix_set = true;
+        flickr_suffix_set = true;
       }
       responsiveImages();
     };
 
-    module.register(submodule.init);
+    module.register(init);
+    module.register(function () {
+      if (module.config.debug) {
+        // Open up some internal items for debugging.
+        submodule.init = init;
+        submodule.imageSrc = imageSrc;
+        submodule.flickr_suffix = function (value) { if (typeof value !== 'undefined') { flickr_suffix = value; } else { return flickr_suffix; }};
+        submodule.flickr_suffix_set = function (value) { if (typeof value !== 'undefined') { flickr_suffix_set = !!(value); } else { return flickr_suffix_set; }};
+      }
+    });
 
     return submodule;
   }(module, module.FLICKR || {}, $));
