8c3ce3f5e9dd7e2a5401aa82cb03cdbf7899e098
Orde Saunders <orde@decadecity.net>
2014-09-23 17:37:48 +0100

    Udpated versioning.

--- Gruntfile.js
+++ Gruntfile.js
@@ -70,7 +70,6 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-qunit');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
